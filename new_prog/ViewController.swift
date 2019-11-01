2b7cfa80fece642e07a9c884a259d9b2050de78f
maan-satveer <koursatveer12@gmail.com>
2019-11-01 13:55:29 -0400

    table view controller

---
+++ new_prog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  new_prog
+//
+//  Created by MacStudent on 2019-11-01.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
